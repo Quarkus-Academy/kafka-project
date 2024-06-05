6eb6d17f7e5ae7b0fb757ee15291d054f98c7997
Petra Jankurová <petra.jankurova@ibm.com>
2024-06-05 16:14:56 +0200

    setting up assignment branch

--- src/main/java/com/ibm/quarkusacademy/Processor.java
+++ src/main/java/com/ibm/quarkusacademy/Processor.java
@@ -1,42 +1,17 @@
 package com.ibm.quarkusacademy;
 
-import java.util.Random;
 import java.util.concurrent.CompletionStage;
-
-import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import io.quarkus.logging.Log;
-
+// consumes messages from "scheduled" topic, converts them to uppercase 
+// then sends the messages to topic "uppercase" and consumes this topic 
 public class Processor {
 
-    @Incoming("scheduled-in")
-    @Outgoing("uppercase-out")
     public Message<String> consumeAndSendMessage (Message<String> message) {
-
-        String event = message.getPayload(); 
-        Log.infof("Incoming scheduled event: %s", event);
-
-        String uppercaseEvent = event.toUpperCase();
-        Log.infof("Outgoing uppercase event: %s", event);
-        return Message.of(uppercaseEvent);
 
     }
 
-    @Incoming("uppercase-in")
     public CompletionStage<Void> consumeMessage (Message<String> message) {
-
-        Random random = new Random(); 
-        if (random.nextBoolean()) {
-            String event = message.getPayload(); 
-            Log.infof("Incoming uppercase event: %s", event);
-            return message.ack();
-        }
-        else {
-            return message.nack(new Exception());
-        }
-        
 
     }
     
--- src/main/java/com/ibm/quarkusacademy/Scheduler.java
+++ src/main/java/com/ibm/quarkusacademy/Scheduler.java
@@ -1,28 +1,17 @@
 package com.ibm.quarkusacademy;
 
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-
-import io.quarkus.logging.Log;
-import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
+// publishes messages to topic "scheduled" every 10 seconds 
 @ApplicationScoped
 public class Scheduler {
 
-    @Inject
-    @Channel("scheduled-out")
-    Emitter<String> emitter;
-
     public void sendMessage(String event) {
-        Log.infof("Sending message: %s", event);
-        emitter.send(event);
+
     }
 
-    @Scheduled(every = "${kafka.message.interval}", delayed = "10s")
     public void sendScheduledMessage() {
-        sendMessage("Test-Message");
+
     }
 
 }
